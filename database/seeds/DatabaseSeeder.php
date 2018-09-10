4dad2aa3894830c18de4ca6925c36c5c0521979e
Alfian Dimas <kidaliez@gmail.com>
2018-09-10 10:03:23 +0700

    migrate table kontaks,add dummy data for kontaks

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        
+        $faker = Faker\Factory::create();
+
+        for($i=0;$i<20;$i++){
+        	DB::table('kontaks')->insert([
+        		'name'	=> $faker->name,
+        		'email'	=> $faker->email,
+        		'address'	=> $faker->address,
+        		'phonenumber'	=> $faker->phonenumber
+        	]);
+        }
     }
 }
